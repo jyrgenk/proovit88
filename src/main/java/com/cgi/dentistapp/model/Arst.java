f5985f01366114caaa1a059cd9f10963f4b4cea7
Jyrgen Koll <kolljurgen@hotmail.com>
2021-04-29 14:25:36 +0300

    restore

---
+++ src/main/java/com/cgi/dentistapp/model/Arst.java
@@ -0,0 +1,10 @@
+package com.cgi.dentistapp.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Arst {
+    private Long id;
+    private final String eesnimi;
+    private final String perekonnanimi;
+}
